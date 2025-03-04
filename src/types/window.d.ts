9814eff38c2332b7e06dacb31976aaefb9c0da27
Tyler Newman <tylerjnewman18@gmail.com>
2025-03-04 07:59:51 -0300

    Remove unused TypeScript configuration and type definition files

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,22 +1,7 @@
 interface Window {
-  electron: {
-    clipboard: {
-      readText: () => string;
-      writeText: (text: string) => void;
-    }
-  };
-  clipboardAPI: {
-    onClipboardContent: (callback: (content: string) => void) => void;
-  };
-  testAPI?: {
-    sendTest: () => void;
-  };
-  test?: {
-    hello: () => string;
-  };
   electronAPI?: {
     getClipboardText: () => string;
     onClipboardUpdate: (callback: (text: string) => void) => void;
     dragWindow: () => void;
   };
-} 
+}
